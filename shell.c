73f840ecd4bcb96b918652005c059c19e9ea9846
KillianRipoche <killian.ripoche@gmail.com>
2025-01-07 09:26:59 +0100

    Change all the code

--- shell.c
+++ shell.c
@@ -1,61 +1,73 @@
 #include "main.h"
 
-int main(int argc, char *argv[])
+int main(void)
 {
-	char *buffer = NULL;
-	size_t buff_size = 0;
-	pid_t child_pid;
-	char *args[2];
-	(void)argc;
-	(void)argv;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
 
 	while (1)
 	{
-		printf("Welcome to my shell\n");
-		printf("type 'exit' to quit\n");
-		printf("$ ");
 
-		if (getline(&buffer, &buff_size, stdin) == -1)
+		if (isatty(STDIN_FILENO))
 		{
-			perror("getline failed");
-			free(buffer);
-			exit(1);
+			printf("$ ");
+			fflush(stdout);
 		}
 
-		buffer[strcspn(buffer, "\n")] = 0;
+		nread = getline(&line, &len, stdin);
 
-		if (strcmp(buffer, "exit") == 0)
+		if (nread == -1)
 		{
-			free(buffer);
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			break;
+		}
+
+		line[nread - 1] = '\0';
+
+		if (strlen(line) == 0)
+			continue;
+
+		if (strcmp(line, "exit") == 0)
+		{
+			free(line);
 			exit(0);
 		}
 
-		child_pid = fork();
-		if (child_pid < 0)
-		{
-			perror("fork failed");
-			free(buffer);
-			exit(1);
-		}
-		else if (child_pid == 0)
-		{
-
-			if (execve(buffer, args, NULL) == -1)
-			{
-				perror(argv[0]);
-				free(buffer);
-				exit(1);
-			}
-		}
-		else
-		{
-			if (wait(NULL) == -1)
-			{
-				perror("wait failed");
-			}
-		}
+		execute_command(line);
 	}
 
-	free(buffer);
+	free(line);
 	return 0;
+}
+
+void execute_command(char *line)
+{
+	pid_t child_pid = fork();
+
+	if (child_pid < 0)
+	{
+		perror("fork failed");
+		return;
+	}
+	else if (child_pid == 0)
+	{
+		char *args[2];
+		args[0] = line;
+		args[1] = NULL;
+
+		if (execve(line, args, NULL) == -1)
+		{
+			perror("execve failed");
+			exit(1);
+		}
+	}
+	else
+	{
+		if (wait(NULL) == -1)
+		{
+			perror("wait failed");
+		}
+	}
 }
