a481898c391387bcb212b081a1448a4db6b08c20
KillianRipoche <killian.ripoche@gmail.com>
2025-01-07 14:03:31 +0100

    Add environ

--- shell.c
+++ shell.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char **environ)
 {
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t Numc_read;
+	ssize_t Num_read;
 
 	(void)argc;
 
@@ -17,16 +17,16 @@ int main(int argc, char *argv[])
 			fflush(stdout);
 		}
 
-		Numc_read = getline(&line, &len, stdin);
+		Num_read = getline(&line, &len, stdin);
 
-		if (Numc_read == -1)
+		if (Num_read == -1)
 		{
 			if (isatty(STDIN_FILENO))
 				printf("\n");
 			break;
 		}
 
-		line[Numc_read - 1] = '\0';
+		line[Num_read - 1] = '\0';
 
 		if (strlen(line) == 0)
 			continue;
@@ -37,20 +37,20 @@ int main(int argc, char *argv[])
 			exit(0);
 		}
 
-		execute_command(line, argv[0]);
+		execute_command(line, argv[0], environ);
 	}
 
 	free(line);
 	return 0;
 }
 
-void execute_command(char *line, char *exec_name)
+void execute_command(char *line, char *exec_name, char **environ)
 {
 	pid_t child_pid = fork();
 
 	if (child_pid < 0)
 	{
-		perror(exec_name);
+		perror("fork failed");
 		return;
 	}
 	else if (child_pid == 0)
@@ -59,9 +59,7 @@ void execute_command(char *line, char *exec_name)
 		args[0] = line;
 		args[1] = NULL;
 
-		free(line);
-
-		if (execve(line, args, NULL) == -1)
+		if (execve(line, args, environ) == -1)
 		{
 			perror(exec_name);
 			exit(1);
@@ -71,7 +69,7 @@ void execute_command(char *line, char *exec_name)
 	{
 		if (wait(NULL) == -1)
 		{
-			perror(exec_name);
+			perror("wait failed");
 		}
 	}
 }
