34beac17390a65a0e356c5f433c3950d549bee6f
KillianRipoche <killian.ripoche@gmail.com>
2025-01-08 13:41:25 +0100

    File path.c

---
+++ _path.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/** */
+char *get_command_path(char *command)
+{
+	char *path, *path_copy, *dir, *full_path;
+	struct stat st;
+
+	if (strchr(command, '/') != NULL)
+	{
+		if (stat(command, &st) == 0)
+			return (strdup(command));
+		return (NULL);
+	}
+
+	path = getenv("PATH");
+	if (!path)
+		return (NULL);
+
+	path_copy = strdup(path);
+	dir = strtok(path_copy, ":");
+
+	while (dir)
+	{
+		full_path = malloc(strlen(dir) + strlen(command) + 2);
+		sprintf(full_path, "%s/%s", dir, command);
+
+		if (stat(full_path, &st) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (NULL);
+}
