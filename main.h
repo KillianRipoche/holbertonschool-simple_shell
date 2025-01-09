7e7f65d556b6535a4369d5c95e4732ec004ad409
HamzaMoinet <moinethamza0@gmail.com>
2025-01-09 10:58:58 +0100

    main.h

--- main.h
+++ main.h
@@ -1,19 +1,21 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
 #define MAX_TOKENS 64
 
-void execute_command(char *command);
+char *build_full_path(char *dir, char *command);
+char *find_path_in_env(char **env);
 char **tokenize_input(char *input);
 void free_args(char **args);
-char *get_command_path(char *command);
+char *get_command_path(char *command, char **env);
+void execute_command(char *command, char **env);
 
 #endif
