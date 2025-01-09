7a829da1796a37e77c2469df7f3aefbd4ed2f002
KillianRipoche <killian.ripoche@gmail.com>
2025-01-09 17:39:59 +0100

    Add com for strtok

--- _strtok.c
+++ _strtok.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
  * tokenize_input - Splits the input into tokens
- * @input: The input string
+ * @input: The input string to tokenize
  *
- * Return: Array of strings (tokens)
+ * Return: A dynamically allocated array of strings (tokens)
+ * or NULL if memory allocation fails
  */
 char **tokenize_input(char *input)
 {
@@ -15,10 +16,13 @@ char **tokenize_input(char *input)
 	if (!tokens)
 		return (NULL);
 
+	/* Tokens are separated by spaces, tabs, and newlines */
 	token = strtok(input, " \t\n");
 
+	/* Loop through the tokens and store them in the array */
 	while (token && token_count < MAX_TOKENS - 1)
 	{
+		/* Duplicate the token and store it in the tokens array */
 		tokens[token_count] = strdup(token);
 		if (!tokens[token_count])
 		{
@@ -26,7 +30,7 @@ char **tokenize_input(char *input)
 			return (NULL);
 		}
 		token_count++;
-		token = strtok(NULL, " \t\n");
+		token = strtok(NULL, " \t\n"); /* Get the next token */
 	}
 
 	tokens[token_count] = NULL;
